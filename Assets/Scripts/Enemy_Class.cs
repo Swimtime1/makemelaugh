2ddac51ba072379eb946eb010d67bad005436c86
StairsStairsStairs <crimpfax@gmail.com>
2024-01-28 00:28:50 -0500

    enemy ai

--- Assets/Scripts/Enemy_Class.cs
+++ Assets/Scripts/Enemy_Class.cs
@@ -6,15 +6,22 @@ using UnityEngine;
 public class Enemy_Class : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private int health;
-    //private enum range {melee = 1.0f, shotgun = 5.0f, pistol = 10.0f, stationary = 0.0f};
-    private int damage;
+    private int health = 100;
+    private int damage = 10;
     private GameObject player;
     private Vector2 followVector;
+    private enum enemy_type {MeatShield, Bruiser, Rusher, Pistol, Shotgun, Machinegun};
+    private Weapon weap_and_reap = new Weapon();
+
+    private bool Aiden_Crossfield_Mode = false;
+
+    [SerializeField] float minRange = 30;
+    [SerializeField] float minRange = 30;
+    [SerializeField] float minShootrange = 30;
+    [SerializeField] float maxShootrange = 30;
 
 
-    public float minRange = 30;
-    public float movespeed = 3;
+    [SerializeField] float movespeed = 3;
     
     // Start is called before the first frame update
     void Start()
@@ -25,26 +32,50 @@ public class Enemy_Class : MonoBehaviour
 
     // Update is called once per frame
 
+    void hit(int Damage){
+        health -= Damage;
+    }
+
+
     // FixedUpdate works off of physics update not on actual frames, won't slow the game down it just skips frames
     void FixedUpdate()
     {
         if (rb == null){
             rb = GetComponent<Rigidbody2D>();
-            player = GameObject.Find("Square");
+            player = GameObject.Find("P1");
         }
-        //rb.velocity = new Vector2 (2f, 2f);
         
-        if (Vector2.Distance(transform.position, player.transform.position) > minRange){
-            //followVector = (player.transform.position - transform.position).normalized * movespeed;
-            //Debug.Log("Text: " + Vector2.Distance(transform.position, player.transform.position));
+        if (max_range < (Vector2.Distance(transform.position, player.transform.position))) {
 
+            // GET CLOSER
+
+
+
+
+            followVector = (player.transform.position - transform.position).normalized * movespeed;
+            //debug.Log("Text: " + Vector2.Distance(transform.position, player.transform.position));
             //rb.velocity = followVector;
         }
-        else {
-            //rb.velocity = Vector2.zero;
+
+        if ((Vector2.Distance(transform.position, player.transform.position) < minRange)) {
+
+            // GET FARTHER
+
         }
 
-        //followVector = Vector2.MoveTowards(transform.position, player.transform.position, 0.06f);
-        //rb.MovePosition(followVector);
+        if (maxShootrange > (Vector2.Distance(transform.position, player.transform.position)) &&
+            (Vector2.Distance(transform.position, player.transform.position) > minShootrange)) {
+
+            rb.velocity = Vector2.zero;
+            Debug.Log("Shoot");
+        }
+        else {
+            cry
+        }
+
+        if (health <= 0){
+            Debug.Log("Dead");
+        }
+
     }
 }
